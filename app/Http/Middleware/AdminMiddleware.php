6e0c61aaf89d1d21147c51f3cdd3e99c27d591a9
Herlambang Kus <34473347+herlambangK@users.noreply.github.com>
2020-12-09 23:42:45 +0700

    penambahan middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // return $next($request);
+
+        $user = auth()->user();
+
+        if( $user->isAdmin()){
+            return $next($request);
+        }
+        return response()->json([
+            'message'=>'Anda Bukan Admin'
+        ]);
+    }
+}
